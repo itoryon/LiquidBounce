f6d47c3f1d374df66605524d3b0aeaa93ebd1c3b
itoryon <226760486+itoryon@users.noreply.github.com>
2026-08-19 14:57:23 +0500

    Add ModuleTridentBoost to ModuleManager

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/ModuleManager.kt
@@ -579,6 +579,7 @@ object ModuleManager : EventListener, Collection<ClientModule> by modules {
             ModuleReverseStep,
             ModuleStrafe,
             ModuleTerrainSpeed,
+            ModuleTridentBoost,
             ModuleVehicleBoost,
             ModuleVehicleControl,
             ModuleSpider,
